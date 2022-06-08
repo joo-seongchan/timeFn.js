b95083d07015cf90d405602c8717add07c0fdd48
joo-seongchan <jskjsj601@gmail.com>
2022-06-08 12:02:41 +0900

    slide

---
+++ js/slide.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.swiper',{
+    loop:true,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        type:'progressbar',
+        clickable:true
+      },
+      scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: true,
+      },
+    //   autoplay: {
+    //     delay: 1000,
+    //   },
+    //   effect: 'fade',
+    //   fadeEffect: {
+    //     crossFade: true
+    //   },
+      effect: 'flip',
+      flipEffect: {
+        slideShadows: false,
+      },
+})
